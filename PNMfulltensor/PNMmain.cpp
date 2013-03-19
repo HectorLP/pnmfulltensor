8c6623562e192d05fb2b1376f939ad3400ae4b9b
PEI LI <lipeihydrogeology@gmail.com>
2013-03-19 01:36:41 +0100

    start to set up the pore-network for simulation

---
+++ PNMfulltensor/PNMmain.cpp
@@ -0,0 +1,17 @@
+/*the main code of pore-network modeling for calculating the full permeability 
+ tensor.
+*/
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+double lognormal ();
+
+int main ()
+{
+    using namespace std;
+    srand(time(NULL));
+    double pore_bodies[];
+    int num_x, num_y, num_y;
+    return 0;
+}
---
+++ PNMfulltensor/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: lipei
+ *
+ * Created on March 19, 2013, 12:44 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
